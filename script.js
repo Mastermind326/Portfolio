2c18969dcb90d5468e51219c5c2c67aec564b6c7
Sean <seanacousins@gmail.com>
2023-04-10 20:20:13 -0500

    java elements defined and classes set

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let paintButton = document.getElementById('#dasamoto');
-let paint = document.getElementsByClassName('.paint');
+let paintButton = document.getElementById('dasamoto');
+let paint = document.querySelectorAll('.paint');
 
-let teaButton = document.getElementById('#tea');
-let tea = document.getElementsByClassName('.tea');
+let teaButton = document.getElementById('tea');
+let tea = document.getElementsByClassName('tea');
 
-let bikeButton = document.getElementById('#bike');
-let bike = document.getElementsByClassName('.bike');
+let bikeButton = document.getElementById('bike');
+let bike = document.getElementsByClassName('bike');
 
 let open = () =>{
-    paint.document.hidden = 'false';
+    paint.style.display = 'block';
 }
 
 paintButton.addEventListener('click',open);

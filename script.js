47137387337bf941feca52c12398990237e1b3b3
Sean <seanacousins@gmail.com>
2023-04-13 19:42:30 -0500

    add collapsing functionality to projects section

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let paintButton = document.getElementById('dasamoto');
-let paint = document.querySelectorAll('.paint');
 
 let teaButton = document.getElementById('tea');
 let tea = document.getElementsByClassName('tea');
@@ -7,8 +6,32 @@ let tea = document.getElementsByClassName('tea');
 let bikeButton = document.getElementById('bike');
 let bike = document.getElementsByClassName('bike');
 
-let open = () =>{
-    paint.style.display = 'block';
-}
+let toggle = () =>{
+    let paint = document.querySelectorAll('.paint');
+    if(paint[0].style.display === 'none'){
+    return paint[0].style.display = 'block';
+    }
+    else{return paint[0].style.display = 'none'}
+};
 
-paintButton.addEventListener('click',open);
+paintButton.addEventListener('click',toggle);
+
+let toggle2 = () =>{
+    let tea = document.querySelectorAll('.tea');
+    if(tea[0].style.display === 'none'){
+    return tea[0].style.display = 'block';
+    }
+    else{return tea[0].style.display = 'none'}
+};
+
+teaButton.addEventListener('click',toggle2);
+
+let toggle3 = () =>{
+    let bike = document.querySelectorAll('.bike');
+    if(bike[0].style.display === 'none'){
+    return bike[0].style.display = 'block';
+    }
+    else{return bike[0].style.display = 'none'}
+};
+
+bikeButton.addEventListener('click',toggle3);
